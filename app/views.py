3b188cc929aeea5fdef669c9163fa02aca4711c0
niklas <niklas.aholm@outlook.com>
2015-10-27 08:02:36 +0100

    fix templates

--- app/views.py
+++ app/views.py
@@ -12,8 +12,7 @@ import urllib2
 @app.route('/')
 @app.route('/index')
 def index():
-	user = {'nickname': 'Bob'}
-	return render_template('index.html', title='Home', user=user)
+	return render_template('index.html')
 
 
 @app.route('/gmshairfoil', methods=['GET', 'POST'])
@@ -32,8 +31,8 @@ def test():
         step = ((angle_stop - angle_start) / angles)
         angle_list = [angle_start + step*x for x in range(angles+1)]
         res = group([gmsh_convert_airfoil.s(str(x), str(x), str(angles),
-                                                    nodes, levels, samples,viscocity,
-                                                    speed, time_step) for x in angle_list])()
+            						nodes, levels, samples,viscocity,
+                                    speed, time_step) for x in angle_list])()
 
 
         return render_template('gmshairfoil.html', form=form)
@@ -45,22 +44,15 @@ def test():
 def gmsh():
 	form = GMSHForm()
 	if form.validate_on_submit():
-		angle_start = str(form.angle_start.data)
-		angle_stop = str(form.angle_stop.data)
-		angles = str(form.angles.data)
+		angle_start = int(form.angle_start.data)
+		angle_stop = int(form.angle_stop.data)
+		angles = int(form.angles.data)
 		nodes = str(form.nodes.data)
 		levels = str(form.levels.data)
-
-		#angle_list = distributeJob(angle_start, angle_stop, angles)
-		#res = group([start_gmsh.s(a, a, 1, nodes, levels) for a in angle_list])
-
-		res = start_gmsh.delay(angle_start, angle_stop, angles, nodes, levels)
-		res.get()
-
-
-		files = glob.glob('/home/ubuntu/msh/*.msh')
-		convert_group = group([convert_msh.s(msh) for msh in files])
-		res = convert_group()
+		step = ((angle_stop - angle_start) / angles)
+		angle_list = [angle_start + step*x for x in range(angles+1)]
+		res = group([start_gmsh.s(str(x), str(x), str(angles), nodes, levels) for x in angle_list])
+		res()
 
 		return redirect('/index')
 
@@ -70,7 +62,6 @@ def gmsh():
 def airfoil():
 	form = AirfoilForm()
 	if form.validate_on_submit():
-		print 'fuckupp'
 		samples = form.samples.data
 		viscocity = form.viscocity.data
 		speed = form.speed.data
@@ -85,23 +76,6 @@ def airfoil():
 	return render_template('airfoil.html', form=form)
 
 
-@app.route('/convert', methods=['POST'])
-def convert():
-	if not request.json or 'convert' not in request.json:
-		abort(400)
-
-	files = glob.glob('/home/ubuntu/msh/*.msh')
-	file = '/home/ubuntu/msh/{}'.format(request.json['convert'])
-	print file
-	print request.json['convert']
-	if request.json['convert'] == 'all':
-		convert_group = group([convert_msh.s(msh) for msh in files])
-		res = convert_group()
-
-	elif file in files:
-		convert_msh.delay(file)
-
-	return redirect('/index')
 
 @app.route('/api/v1.0/<xml>', methods=['GET'])
 def get_xml_file(xml):
--- task.py
+++ task.py
@@ -46,6 +46,16 @@ def start_gmsh(angle_start, angle_stop, angles, nodes, levels):
     call('sudo chown -R ubuntu /home/ubuntu/msh', shell=True)
     call('sudo chown -R ubuntu /home/ubuntu/geo', shell=True)
 
+    for level in range(int(levels) + 1):
+        path = '/home/ubuntu/msh/'
+        filename = '/home/ubuntu/msh/r{}a{}n{}.msh'.format(level, angle_start, nodes)
+        filename_xml = filename[:-3]
+        filename_xml += 'xml'
+       	check_call('sudo dolfin-convert {} {}'.format(filename, filename_xml), shell=True)
+
+        with open(filename_xml) as f:
+            conn.put_object('g17container', filename_xml[len(path):],
+                            contents=f.read(), content_type='text/plain')
 
 @celery.task
 def convert_msh(filename):
@@ -55,5 +65,21 @@ def convert_msh(filename):
 
 @celery.task
 def start_airfoil(samples, viscocity, speed, time_step, filename):
-	filename = '/home/ubuntu/msh/{}'.format(filename)
-	call(['sudo', '/home/ubuntu/naca_airfoil/navier_stokes_solver/airfoil', samples, viscocity, speed, time_step, filename])
+    filename = '/home/ubuntu/msh/{}'.format(filename)
+    #call(['sudo', '/home/ubuntu/naca_airfoil/navier_stokes_solver/airfoil', samples, viscocity, speed, time_step, filename])
+
+    airfoil_path = '/home/ubuntu/naca_airfoil/navier_stokes_solver/airfoil'
+
+    try:
+        check_call(['sudo', '/home/ubuntu/naca_airfoil/navier_stokes_solver/airfoil',
+                    samples, viscocity, speed, time_step, filename])
+
+    except CalledProcessError as e:
+        print e
+
+    filename = '{}/s{}v{}s{}t{}/drag_lift.m'.format(filename_xml[len(path):],
+                                        samples, viscocity, speed, time_step)
+
+    with open('/home/ubuntu/test/results/drag_ligt.m') as f:
+        conn.put_object('g17container', filename,
+                        contents=f.read(), content_type='text/plain')
